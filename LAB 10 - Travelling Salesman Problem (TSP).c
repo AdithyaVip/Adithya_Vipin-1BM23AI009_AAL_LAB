614e80d0325dc6a55d71ed023088bfb78e4f7fe6
Adithya Vipin <adithyavipinakumar@gmail.com>
2025-06-01 16:12:50 +0530

    Update LAB 10 - Travelling Salesman Problem (TSP).c

--- LAB 10 - Travelling Salesman Problem (TSP).c
+++ LAB 10 - Travelling Salesman Problem (TSP).c
@@ -1,49 +1,66 @@
 INPUT -
-    
 #include <stdio.h>
 #include <limits.h>
 
 #define V 4
 
-int tsp(int graph[V][V], int visited[], int pos, int n, int count, int cost, int start) {
-    if (count == n && graph[pos][start]) {
-        return cost + graph[pos][start];
+int graph[V][V] = {
+    {0, 10, 15, 20},
+    {10, 0, 35, 25},
+    {15, 35, 0, 30},
+    {20, 25, 30, 0}
+};
+
+int minCost = INT_MAX;
+int bestPath[V + 1];
+
+void tsp(int city, int visited[], int path[], int count, int cost) {
+    if (count == V) {
+        if (graph[city][0]) { // Can return to start?
+            int totalCost = cost + graph[city][0];
+            if (totalCost < minCost) {
+                for (int i = 0; i < V; i++) bestPath[i] = path[i];
+                bestPath[V] = 0;
+                minCost = totalCost;
+            }
+        }
+        return;
     }
 
-    int ans = INT_MAX;
-
-    for (int i = 0; i < n; i++) {
-        if (!visited[i] && graph[pos][i]) {
-            visited[i] = 1;
-            int temp = tsp(graph, visited, i, n, count + 1, cost + graph[pos][i], start);
-            ans = (temp < ans) ? temp : ans;
-            visited[i] = 0;
+    for (int i = 0; i < V; i++) {
+        if (!visited[i] && graph[city][i]) {
+            int nextCost = cost + graph[city][i];
+            if (nextCost < minCost) { // Prune: skip if already worse
+                visited[i] = 1;
+                path[count] = i;
+                tsp(i, visited, path, count + 1, nextCost);
+                visited[i] = 0; // Backtrack
+            }
         }
     }
-
-    return ans;
 }
 
 int main() {
-    int graph[V][V] = {
-        {0, 10, 15, 20},
-        {10, 0, 35, 25},
-        {15, 35, 0, 30},
-        {20, 25, 30, 0}
-    };
-
-    int visited[V];
-    for (int i = 0; i < V; i++) visited[i] = 0;
+    int visited[V] = {0};
+    int path[V + 1];
 
     visited[0] = 1;
+    path[0] = 0;
 
-    int result = tsp(graph, visited, 0, V, 1, 0, 0);
-    printf("Minimum cost of travelling all cities: %d\n", result);
+    tsp(0, visited, path, 1, 0);
+
+    printf("Minimum cost: %d\nPath: ", minCost);
+    for (int i = 0; i <= V; i++)
+        printf("%d ", bestPath[i]);
+    printf("\n");
 
     return 0;
 }
 
 
+
  OUTPUT -
 
-Minimum cost of travelling all cities: 80
+Minimum cost: 80
+Path: 0 1 3 2 0 
+
